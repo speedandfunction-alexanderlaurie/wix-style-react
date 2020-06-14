a0b4dd0919227544d466248d743559968bac26ac
TalZa <24567980+TalZa1810@users.noreply.github.com>
2020-06-14 15:27:09 +0300

    removing classnames from TabItem

--- src/Tabs/core/TabItem/index.js
+++ src/Tabs/core/TabItem/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
 import pick from '../../../utils/operators/pick';
 import Text from '../../../Text';
 import * as TabPropTypes from '../constants/tab-prop-types';
@@ -24,7 +23,7 @@ class TabItem extends React.Component {
     const containerProps = {
       key: item.id,
       onClick: () => onItemClick(item),
-      className: classNames(styles.tab, { [styles.active]: isActive }),
+      ...styles('tab', { active: isActive }),
       style: this.getItemStyle(),
       ...pick(this.props, dynamicProperties),
     };
